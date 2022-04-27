9d099b6e794b5d447241650a1bb461e12113353c
Giulio <gaotar@gmail.com>
2022-04-27 22:49:29 +0200

    working gps

--- MCU/MCU.ino
+++ MCU/MCU.ino
@@ -3,35 +3,66 @@
 #include <SPI.h>
 #include <Adafruit_BME280.h>
 
+
+//----------  TEMPERATURE SENSOR ---------------------
 Adafruit_BME280 bme; // use I2C interface
 Adafruit_Sensor *bme_temp = bme.getTemperatureSensor();
 Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
 Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
 
+
 float temp, humi, pres;
-//int ledPin=2;
 
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
+//---------- GPS RECEIVER -------------------------------
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+TinyGPSPlus gps;
+SoftwareSerial GPSerial(12,13);
+float Latitude , Longitude;
+int year , month , date, hour , minute , second;
+
+
+
+//----------- WI-FI SECTION ---------------------------
 
 #include <ESP8266WiFi.h>
-//#include <HTTPClient.h>
+
+
+char ssid[] = "Brucewillis'";
+char pass[] = "quiprende!";
+
+
+//------------ HTTP SECTION ----------------------------
 #include <ESP8266HTTPClient.h>
 WiFiClient client;
+char serverURL[] = "http://192.168.1.139:80/getSensorValue.php";
 
-char ssid[] = "Gruppo14";
-char pass[] = "12345678";
-char serverURL[] = "http://192.168.177.210:80/getSensorValue.php";
 
+
+//  FUNCTIONS
+
+
+//--------------- GET SENSOR READING ---------------------
 void SensorRead() {
+
+  //----------------------- CLIMATE
   temp=bme.readTemperature();
   humi=bme.readHumidity();
   pres=bme.readPressure()/100.0F;
   Serial.print("temp = ");  // Print the results...
   Serial.println(temp);  // ...to the serial monitor:
+
+
+  //----------------------- COORDINATES
+  gps.encode(GPSerial.read());
+  Serial.print("Longitude = ");
+  Longitude=gps.location.lng();
+  year=gps.date.year();
 }
 
+//--------------- SEND RESULTS TO SERVER
 void sendToServer(String sensorData){
   HTTPClient http; //un oggetto http
   if (WiFi.status() != WL_CONNECTED){
@@ -46,11 +77,17 @@ void sendToServer(String sensorData){
   }
 }
 
+
 void setup()
 {
   // Debug console
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("Qua ci arrivo");
+  
+  //GPS Serial
+  GPSerial.begin(9600);
 
+  //Check temperature sensor
   if (!bme.begin(0x76) ){
     Serial.println("Fallimento");
     while(1) delay (1000);
@@ -60,7 +97,13 @@ void setup()
     bme_humidity->printSensorDetails();
   }
 
-  //setup connection
+
+  //Let's catch GPS signal!
+  while(GPSerial.available()<=0){
+    Serial.println("Waiting for gps");
+  }
+
+  //setup connection TODO chip will work when offline
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -73,9 +116,9 @@ void setup()
 void loop()
 {
   SensorRead();
-  String sensorData="humidity="+String(humi)+"&temperature="+String(temp)+"&pressure="+String(pres);
+  String sensorData="humidity="+String(humi)+"&temperature="+String(temp)+"&pressure="+String(pres)+"&year="+String(year)+"&longitude="+String(Longitude);
   Serial.println(sensorData);
   sendToServer(sensorData);
-  delay(5000);  
+  delay(60000);  
   
 }
--- WEB/getSensorValue.php
+++ WEB/getSensorValue.php
@@ -3,10 +3,13 @@
         <title>Una pagina opensource </title>
     </head>
     <body>
-    <?php $f = fopen("file.txt", "w");
+    <?php
+    $f = fopen("file.txt", "w");
     fwrite($f, $_POST["temperature"]."\n");
     fwrite($f, $_POST["humidity"]."\n");
-    fwrite($f, $_POST["pressure"]);
+    fwrite($f, $_POST["pressure"]."\n");
+    fwrite($f, $_POST["year"]."\n");
+    fwrite($f, $_POST["longitude"]);
     fclose($f);
     ?>
     <?php echo "Hello World! <p>"; ?>
