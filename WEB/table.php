f89c18592f66f70ba2ec9bb98b67f351a18f67d0
Giulio <gaotar@gmail.com>
2022-06-21 00:57:47 +0200

    Export to excel and remove entries

--- WEB/table.php
+++ WEB/table.php
@@ -65,10 +65,11 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
   </header>
 
   <div class="w3-responsive" style="padding:20px">
-	<table class="w3-table-all">
+	  <table class="w3-table-all">
+
     	<tr> 
-        	<th> Date </th> <th> Time </th> <th> Latitude </th> <th> Longitude </th> <th> Temperature </th> <th> Humidity </th> <th> Pressure </th>
-        </tr>
+        	<th> Date </th> <th> Time </th> <th> Latitude </th> <th> Longitude </th> <th> Temperature </th> <th> Humidity </th> <th> Pressure </th> <th> Device </th> <th class='w3-hide'> id </th>
+      </tr>
 
 
 <?php
@@ -82,8 +83,37 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
       die("Connessione fallita: " . mysqli_connect_error());
   }
 
+
+  if(isset($_GET["action"]) && $_GET["action"]==="delete" && isset($_POST["DeletionQuery"]) ){
+   
+    $giantString=$_POST["DeletionQuery"];
+
+    // ;2022-06-13_05-47-08:verylongid STRING PROTOTYPE
+
+    $badlyFormattedKeys=explode(';',$giantString);
+    $Queries='';
+   
+    foreach($badlyFormattedKeys as $index => $key){
+      if( $index > 0) {  //first string is null because the semicolon precedes every string
+        $fields=explode(':', $key);
+        
+        $Queries.='(device_id="'.$fields[1].'" AND Timestamp=STR_TO_DATE("'.$fields[0].'", "%Y-%m-%d_%H-%i-%s") )';
+        
+        if( $index !== array_key_last($badlyFormattedKeys) ){
+          $Queries.=' OR ';
+        }
+      }
+    }
+    $QueryString='DELETE FROM Climate WHERE '.$Queries;
+
+    //DELETE FROM Climate WHERE (device_id=$fields[1] AND Timestamp=STR_TO_DATE($fileds[0], "%Y-%m-%d_%H-%i-%s") ) OR (device_id=.. AND Timestap=.. )
     
-  $query='SELECT Timestamp, Latitude, Longitude, Temperature, Pressure, Humidity FROM Climate WHERE device_id IN (SELECT id FROM Registered_Devices WHERE User="utente01@esempio.it")';
+    mysqli_query($conn, $QueryString);
+    
+  }
+
+    
+  $query='SELECT Timestamp, Latitude, Longitude, Temperature, Pressure, Humidity, device_name as Device, id  FROM Climate, Registered_Devices WHERE Climate.device_id=Registered_Devices.id AND Registered_Devices.User="utente01@esempio.it"';
   $result=mysqli_query($conn, $query );
 
 
@@ -98,6 +128,8 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
         echo "<td>".$row["Temperature"]."</td>";
         echo "<td>".$row["Humidity"]."</td>"; 
         echo "<td>".$row["Pressure"]."</td>";
+        echo "<td>".$row["Device"]."</td>";
+        echo "<td class='w3-hide'>".$row["id"]."</td>";
       echo "\n</tr>\n";
     }
   }
@@ -105,10 +137,15 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 
         
 ?>
-        
+
 	</table>
-  </div> 
-  
+    <div class="w3-bar w3-padding-24">
+      <button class="w3-button w3-black w3-left" onclick="selectToExport()">Export</button>
+      <button class="w3-button w3-black w3-right" onclick="selectToRemove()">Clear</button>
+      <div class="w3-bar-element w3-center"></div>
+    </div>
+  </div>
+
   <br>
   <div class="w3-container w3-dark-grey w3-padding-32">
     <div class="w3-row">
@@ -166,8 +203,172 @@ function w3_close() {
   mySidebar.style.display = "none";
   overlayBg.style.display = "none";
 }
+
+var selected=[];
+
+function selectToExport() {
+  Array.from(document.getElementsByTagName("tr")).forEach((element,i) => i ? element.prepend(createElementCheckbox()) : element.prepend(createHeaderCheckbox()));
+
+  document.getElementsByClassName("w3-button w3-black w3-left")[0].setAttribute("disabled", "");
+  document.getElementsByClassName("w3-button w3-black w3-right")[0].setAttribute("disabled","");
+  document.getElementsByClassName("w3-bar-element w3-center")[0].innerHTML='<button class="w3-button w3-orange" onClick="exportSelected()">Export</div>';
+
+  selected=[];
+}
+
+function selectToRemove(){
+  Array.from(document.getElementsByTagName("tr")).forEach((element,i) => i ? element.prepend(createElementCheckbox()) : element.prepend(createHeaderCheckbox()));
+
+  document.getElementsByClassName("w3-button w3-black w3-left")[0].setAttribute("disabled", "");
+  document.getElementsByClassName("w3-button w3-black w3-right")[0].setAttribute("disabled","");
+  document.getElementsByClassName("w3-bar-element w3-center")[0].innerHTML='<button class="w3-button w3-red" onClick="deleteSelected()">Delete</div>';
+
+  selected=[];
+}
+
+function deleteSelected(){
+  
+  //undo UI changes
+ 
+  // Array.from(document.getElementsByTagName("tr")).forEach((element) => element.childNodes[0].remove());
+
+  // document.getElementsByClassName("w3-button w3-black w3-left")[0].removeAttribute("disabled");
+  // document.getElementsByClassName("w3-button w3-black w3-right")[0].removeAttribute("disabled");
+  // document.getElementsByClassName("w3-bar-element w3-center")[0].innerHTML='';
+
+  //build queryString
+  if(selected.length>0){
+    queryString='';
+    selected.forEach(
+      function(element){
+        queryString+=';'+element.childNodes[2].innerHTML;
+        queryString+='_'+element.childNodes[3].innerHTML.replace(/:/g, "-");
+        queryString+=':'+element.childNodes[element.childNodes.length-2].innerHTML;
+      }
+    );
+
+    //send query to php
+
+    const form = document.createElement('form');
+    form.method="post";
+    form.action=window.location.href+"?action=delete";
+
+    const input= document.createElement("input");
+    input.name="DeletionQuery";
+    input.value=queryString;
+    input.type="hidden";
+
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
+  }
+}
+
+function createElementCheckbox() {
+  const elementCheckbox=document.createElement("input");
+  elementCheckbox.setAttribute("type", "checkbox");
+  elementCheckbox.setAttribute("class", "w3-check w3-margin-left elementcheck");
+  elementCheckbox.setAttribute("onClick", "toggleSelect(this)");
+  return elementCheckbox;
+}
+
+function createHeaderCheckbox() {
+  const elementCheckbox=document.createElement("input");
+  elementCheckbox.setAttribute("type", "checkbox");
+  elementCheckbox.setAttribute("class", "w3-check w3-margin-left headercheck");
+  elementCheckbox.setAttribute("onClick", "toggleSelectAll(this)");
+  return elementCheckbox;
+}
+
+function exportSelected() {
+  
+
+  //undo UI changes
+ 
+  Array.from(document.getElementsByTagName("tr")).forEach((element) => element.childNodes[0].remove());
+
+  document.getElementsByClassName("w3-button w3-black w3-left")[0].removeAttribute("disabled");
+  document.getElementsByClassName("w3-button w3-black w3-right")[0].removeAttribute("disabled");
+  document.getElementsByClassName("w3-bar-element w3-center")[0].innerHTML='';
+
+  // export
+
+  // outputString='';
+  // selected.forEach( element => outputString+=";"+element);
+  // alert(outputString);
+  if(selected.length>0){
+    createXSL();
+    selected=[];
+  }
+}
+
+function createXSL(){
+
+  const exportabletable=document.createElement("table");
+  const header=document.getElementsByTagName("th")[0].closest("tr").cloneNode(true);
+  header.childNodes[header.childNodes.length-1].remove();
+  header.childNodes[header.childNodes.length-1].remove();
+  exportabletable.append(header);
+  selected.forEach( 
+    function(element) {
+      //clone selected row
+      row=element.cloneNode(true);
+
+      //put it into the exportabletable
+      exportabletable.append(row) ;
+
+      //delete the id string
+      row.childNodes[row.childNodes.length-1].remove();
+      row.childNodes[row.childNodes.length-1].remove();
+
+    }
+  );
+  var downloadLink;
+  var dataType = 'application/vnd.ms-excel';
+  var tableHTML = exportabletable.outerHTML.replace(/ /g, '%20');
+  filename="data.xls";
+  downloadLink = document.createElement("a");
+
+  document.body.appendChild(downloadLink);
+  downloadLink.href = "data:"+dataType+', '+tableHTML;
+  downloadLink.download=filename;
+  downloadLink.click();
+
+}
+
+function toggleSelect(obj) {
+
+
+  const row=obj.closest("tr");
+  index=selected.indexOf(row);
+  if(index<0) selected.push(row);
+  else selected.splice(index, 1);
+
+}
+
+function toggleSelectAll(obj){
+  if (selected.length>0) {
+    while(selected.length>0){
+      selected.shift().childNodes[0].checked=false;
+    }
+    obj.checked=false;
+  }
+  else {
+    rows=Array.from(document.getElementsByTagName("tr"));
+    rows.shift();
+    rows.forEach(
+      function(element, i){
+        
+          element.childNodes[0].checked=true;
+          selected.push(element);
+        
+      }
+    );
+    obj.checked=true;
+  }
+}
+
 </script>
 
 </body>
 </html>
-
